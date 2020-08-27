920fc23fc72224b07f7d5d1b83fd8e9acf84c702
Ryan McNair <ryan.mcnair@gmail.com>
2020-08-27 14:57:57 -0500

    fixed dino image issue, added buttons and progress bar to cards

--- javascripts/components/dinoCard.js
+++ javascripts/components/dinoCard.js
@@ -27,18 +27,30 @@ const addDino = () => {
 
 const makeDinoCard = () => {
     $('#kennel').html("");
-    dinos.forEach((item) => {
+    dinos.forEach((item, index) => {
         $('#kennel').append(`
-        <div class="card" style="width: 18rem;">
-          <div class="img-container" style="background-image: url(${item.imageUrl})">
+        <div class="card m-2" style="width: 18rem;">
+        <img class="card-img-top" src="${item.imageUrl}" alt="Card image cap">
+        <div class="card-body">
+          <div class="card-body">
+              <h5 class="card-title">${item.name}</h5>
+              <div class="progress" id="progressBar-${item.id}">
+              <div class="progress-bar progress-bar ${
+                item.health > 50 ? "bg-success" : "bg-warning"
+              } progress-bar-animated" style="width: ${item.health}%" id="progress-${item.id}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"><p class="health-status">${item.health}%</p></div>
           </div>
-              
-              <div class="card-body">
-                <h2 class="card-title">${item.name}</h2>
-                <p class="card-text"></p>
+          <div class="button-section-${item.id}" id="button-section-${item.id}">
+              <button type="button" id="pet-${item.id}" class="btn btn-success">Pet</button>
+              <button type="button" id="feed-${item.id}" class="btn btn-success">Feed</button>
+              <button type="button" id="adventure-${item.id}" class="btn btn-info">Adventure</button>
+              <button type="button1" id="delete-${item.id}" class="btn btn-danger">Delete</button>
+              <div class="infoButton">
+                <button type="button" id="info-${item.id}" class="btn btn-light" data-toggle="modal" data-target="#exampleModal-${item.id}">
+                Info
+                </button>
+                </div>
               </div>
-             
-          </div>
+              </div>
         `)
     })
 }
--- javascripts/components/navbar.js
+++ javascripts/components/navbar.js
@@ -4,7 +4,7 @@ const navigation = () => {
     $('#navbar').html(`
     <div class="jumbotron">
         <h1 class="display-4">DINO KENNEL</h1>
-        <p class="lead">Drop off your dino by adding him to the kennel by clicking the button below. Dinos love to be petted and fed! Be sure to take good care of them or they'll end up in the graveyard.</p>
+        <p class="lead">Drop off your dino by adding him to the kennel by clicking the button below. If your dino is good, feed her or give her a pet! Be sure to take good care of them or they'll end up in the graveyard.</p>
         <hr class="my-4">
         <p></p>
         <a class="btn btn-warning btn-lg" role="button" id="add-dino">Add a dino!</a>
